7a2b20687ed691c711578fea1f4f0c1a09c49cf6
Nobuyuki Izumi <nobu.izumi.1212@gmail.com>
2015-10-23 21:00:48 +0900

    InitialData完全版（48都道府県）登録 SecondActivity: switch文も完全板に修正

--- app/src/main/java/com/ultimathule/ultima_project1/InitialData.java
+++ app/src/main/java/com/ultimathule/ultima_project1/InitialData.java
@@ -5,7 +5,22 @@ package com.ultimathule.ultima_project1;
  */
 public class InitialData {
 
-    static Integer[] initialDataIds = {1, 2, 3};
-    static String[] initialDataStates = {"Tokyo", "Saitama Prefecture", "Miyagi prefecture"};
+    static Integer[] initialDataIds =
+            {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24,
+                    25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+                    45, 46, 47};
+
+    static String[] initialDataStates = {"Hokkaido Prefecture", "Aomori Prefecture", "Iwate Prefecture",
+            "Miyagi Prefecture", "Akita Prefecture", "Yamagata Prefecture", "Fukushima Prefecture",
+            "Ibaraki Prefecture", "Tochigi Prefecture", "Gunma Prefecture", "Saitama Prefecture",
+            "Chiba Prefecture", "Tokyo", "Kanagawa Prefecture", "Yamanashi Prefecture", "Niigata Prefecture",
+            "Toyama Prefecture", "Ishikawa Prefecture", "Fukui Prefecture", "Nagano Prefecture",
+            "Gifu Prefecture", "Shizuoka Prefecture", "Aichi Prefecture", "Mie Prefecture",
+            "Shiga Prefecture", "Kyoto Prefecture", "Osaka Prefecture", "Hyogo Prefecture",
+            "Nara Prefecture", "Wakayama Prefecture", "Tottori Prefecture", "Shimane Prefecture",
+            "Okayama Prefecture", "Hiroshima Prefecture", "Yamaguchi Prefecture", "Tokushima Prefecture",
+            "Kagawa Prefecture", "Ehime Prefecture", "Kochi Prefecture", "Fukuoka Prefecture",
+            "Saga Prefecture", "Nagasaki Prefecture", "Kumamoto Prefecture", "Oita Prefecture",
+            "Miyazaki Prefecture", "Kagoshima Prefecture", "Okinawa Prefecture"};
 
 }
